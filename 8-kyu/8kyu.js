a67be93749c59c299eaef6cb6cc604ab6c4daa34
codeHarvest <madhusudanbhardwaj94@gmail.com>
2019-10-25 22:37:18 +0530

    more 8 kyu problems

--- 8-kyu/8kyu.js
+++ 8-kyu/8kyu.js
@@ -69,3 +69,52 @@ const abbrevName = (name) => {
 function boolToWord( bool ){
     return bool === true ? 'Yes':'No';
   }
+
+  // invert values 
+  
+  function invert(array) {
+   return array.map(i => 0 - i);
+  }
+
+  // string repeat
+
+  function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+  // convert bin to decimal
+
+  function binToDec(bin){
+    return parseInt(bin,2);
+  }
+
+  // return the day
+
+  function whatday(num) { 
+    switch(num) {
+      case 1:
+        return 'Sunday';
+        break;
+      case 2:
+        return 'Monday';
+        break;
+      case 3:
+        return 'Tuesday';
+        break;
+      case 4:
+        return 'Wednesday';
+        break;
+      case 5:
+        return 'Thursday';
+        break;
+      case 6:
+        return 'Friday';
+        break;
+      case 7:
+        return 'Saturday';
+        break;
+      default:
+        return  "Wrong, please enter a number between 1 and 7"
+    }
+  }
+
