84a4c89699708e4adb503a5cb3ec7807df7fd561
codeHarvest <madhusudanbhardwaj94@gmail.com>
2019-10-28 23:47:14 +0530

    7kyu problems

---
+++ 7Kyu.js
@@ -0,0 +1,26 @@
+// Descending Order
+
+function descendingOrder(n){
+    return parseInt(String(n).split('').sort().reverse().join(''))
+  }
+
+// sum of two lowest positive integer 
+
+function sumTwoSmallestNumbers(numbers) {  
+    numbers.sort((a,b) => a - b);
+    return numbers[0] + numbers[1];
+  };
+
+
+// Vowel Count
+
+function getCount(str) {
+    return str.replace(/[^aeiou]/gi, '').length;
+  }
+
+// Highest and lowest
+
+function highAndLow(numbers){
+    var arr = numbers.split(' ').sort(function(a, b) { return a - b });
+    return arr[arr.length -1] + ' ' + arr[0];
+  }
