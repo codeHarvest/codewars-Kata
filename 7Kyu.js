6fef75e46b6aae1f5e5798ab9a93b4af272a7e69
codeHarvest <madhusudanbhardwaj94@gmail.com>
2019-10-29 23:12:34 +0530

    more 7kyu problems

--- 7Kyu.js
+++ 7Kyu.js
@@ -24,3 +24,18 @@ function highAndLow(numbers){
     var arr = numbers.split(' ').sort(function(a, b) { return a - b });
     return arr[arr.length -1] + ' ' + arr[0];
   }
+
+
+// Binary addition
+
+function addBinary(a,b){
+  return (a+b).toString(2)
+}
+
+// shortest word
+
+function findShort(s){
+  return s.split(" ").map(word => word.length).sort((a,b) => a - b)[0]
+}
+
+//
